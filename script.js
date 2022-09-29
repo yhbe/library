6d11858ce59a4cf017565f2b6496392267951dc3
SHEF <Shefqetmush@gmail.com>
2022-09-29 12:47:25 -0400

    working on functions

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 let myLibrary = [];
 
 function Book(book) {
-  this.author = author;
-  this.title = title;
-  this.pages = pages;
+  this.author = book.author;
+  this.title = book.title;
+  this.pages = book.pages;
 }
 
 function addBookToLibrary() {
-  let info = "";
+  let info = {};
   info.title = document.querySelector("#title").value;
   info.author = document.querySelector("#author").value;
   info.pages = document.querySelector("#pages").value;
@@ -15,3 +15,10 @@ function addBookToLibrary() {
   myLibrary.push(book);
   console.log(book, myLibrary);
 }
+
+const form = document.querySelector("form");
+const formButton = document.querySelector("#formButton");
+form.addEventListener("submit", addBookToLibrary);
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+});
