8771ed5c6100584bae9eca3e03c80c816df90803
SHEF <Shefqetmush@gmail.com>
2022-09-30 22:47:30 -0400

    workingon removing book function

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ function Book(book) {
   this.title = book.title || "Unknown";
   this.author = book.author || "Unknown";
   this.pages = book.pages || "Unknown";
+  this.id = Math.random();
 }
 
 function addBookToLibrary() {
@@ -20,11 +21,24 @@ function addBookToPage(book) {
   let cardContainerDiv = document.querySelector(".cardContainer");
   let div = document.createElement("div");
   div.setAttribute("class", "card");
-  div.innerHTML = `<p> Title: ${book.title} </p> <p> Author: ${book.author} </p> <p> Pages: ${book.pages} </p> <p class="delete">x</p>`;
+  div.innerHTML = `<p> Title: ${book.title} </p> <p> Author: ${book.author} </p> <p> Pages: ${book.pages} </p> <button class="delete" id="${book.id}">x</butt>`;
   cardContainerDiv.appendChild(div);
 }
-const deleteButton = document.querySelector(".deleteButton");
-deleteButton.addEventListener("click", removeBook);
+
+document.body.addEventListener("click", removeBook);
+
+function removeBook(event) {
+  if (event.target.classList[0] == "delete") {
+    let id = Number(event.target.id);
+    console.log(id);
+    const newLibrary = myLibrary.filter((book) => book.id !== id);
+    return (
+      event.target.parentNode.remove(),
+      (myLibrary = newLibrary),
+      console.log(myLibrary)
+    );
+  }
+}
 
 const form = document.querySelector("form");
 const formButton = document.querySelector("#formButton");
